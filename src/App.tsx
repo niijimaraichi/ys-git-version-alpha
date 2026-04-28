2e8da09c8f727a5cd041c270b361cbc01e0624f4
n97rai <n97rai0822@gmail.com>
2026-04-28 12:11:49 +0900

    TodoApp02ファイルとcomponentsファイルを変更

--- src/App.tsx
+++ src/App.tsx
@@ -189,3 +189,35 @@ function App() {
 }
 
 export default App;
+
+import React from "react";
+import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
+
+import { InputForm } from "./components/InputForm";
+import { List } from "./components/List";
+
+const StyledContainer = styled.div`
+  width: 327px;
+  margin: 24px auto;
+`;
+
+export type TodoList = {
+  id: string;
+  content: string;
+  isCompleted: boolean;
+}
+
+function App() {
+  const [todoList, setTodoList] = React.useState<TodoList[]>([]);
+
+  return (
+    <StyledContainer>
+      <Typography variant="h1" sx={{fontSize: "32px"}}>今日やること</Typography>
+      <InputForm setTodoList={setTodoList} todoList={todoList} />
+      <List setTodoList={setTodoList} todoList={todoList} />
+    </StyledContainer>
+  )
+}
+
+export default App;
